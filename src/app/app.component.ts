de1b072a8ad917be1657fa003cd213a9f64eded3
Wytse Pechler <wytse.pechler@gmail.com>
2024-06-26 12:09:12 +0200

    feature: WIP Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RouterOutlet } from '@angular/router';
+import {AlertComponent} from "./components/message/message.component";
 import {EnvironmentService} from "./services/environment.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, AlertComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
