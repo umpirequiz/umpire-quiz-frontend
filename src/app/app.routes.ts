da15794defd7c9969b4cb681e546e39b2b34df2c
Bram Janssens <s.a.janssens@gmail.com>
2025-01-20 10:06:00 +0100

    First.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,13 @@ import {ResultsComponent} from "./components/quiz/results/results.component";
 import {QuestionComponent} from "./components/admin/question/question.component";
 import {authGuard} from "./guards/auth.guard";
 import {AdminComponent} from "./components/admin/admin.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'contact', component: ContactComponent},
   {
     path: 'quiz', component: QuizComponent,
     children: [
