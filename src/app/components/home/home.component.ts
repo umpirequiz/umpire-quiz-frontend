870854ed82f1a6279febdf0783d39df3394c2c6a
Bram Janssens <s.a.janssens@gmail.com>
2025-02-05 20:50:10 +0100

    try...

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [
-        RouterLink,
-        RouterLinkActive
-    ],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  header = $localize`:site header|An introduction header for this sample:Hello i18n!`;
 
 }
