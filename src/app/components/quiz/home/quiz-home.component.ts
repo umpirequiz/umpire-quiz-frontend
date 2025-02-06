f0384a017928a4518cc2113b2ef2261adc1edffa
Bram Janssens <s.a.janssens@gmail.com>
2025-02-06 21:34:43 +0100

    Show diff level at start quiz.

--- src/app/components/quiz/home/quiz-home.component.ts
+++ src/app/components/quiz/home/quiz-home.component.ts
@@ -1,17 +1,29 @@
 import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {QuizService} from "../../../services/quiz.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+interface Level {
+  u1: boolean
+  u2: boolean
+  u3: boolean
+  u4: boolean
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './quiz-home.component.html',
   styleUrl: './quiz-home.component.scss'
 })
 export class QuizHomeComponent {
+  level = {u1: false, u2: false, u3: false, u4: false};
+
   constructor(private quizService: QuizService) {
   }
 
