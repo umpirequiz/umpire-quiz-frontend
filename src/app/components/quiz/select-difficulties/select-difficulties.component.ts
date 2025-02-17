9054cae6f03eb0d3473fb729b1ddf5304bf1a4b2
Bram Janssens <s.a.janssens@gmail.com>
2025-02-17 22:11:06 +0100

    Some UI improvements at Difficulties.

--- src/app/components/quiz/select-difficulties/select-difficulties.component.ts
+++ src/app/components/quiz/select-difficulties/select-difficulties.component.ts
@@ -2,12 +2,16 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {fromDiff, Levels} from "../../../domain/Levels";
 import {FormsModule} from "@angular/forms";
 import {Difficulty, u1, u2, u3, u4} from "../../../domain/Difficulty";
+import {MatTooltip} from "@angular/material/tooltip";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-select-difficulties',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatTooltip,
+    MatIcon
   ],
   templateUrl: './select-difficulties.component.html',
   styleUrl: './select-difficulties.component.scss'
@@ -17,6 +21,9 @@ export class SelectDifficultiesComponent {
   @Input() levels: Levels = {u1: false, u2: false, u3: false, u4: false}
   @Input() admin = false;
   @Output() choose = new EventEmitter<Difficulty>();
+
+  tooltipText = "Corresponds to U1-U4 levels (1=beginner, 4=veteran)";
+  tooltipVisible = false;
 
   select(selectedLevel: Difficulty) {
     if (this.admin) {
@@ -29,4 +36,9 @@ export class SelectDifficultiesComponent {
   protected readonly u2 = u2;
   protected readonly u3 = u3;
   protected readonly u4 = u4;
+
+
+  toggle() {
+    this.tooltipVisible = !this.tooltipVisible
+  }
 }
